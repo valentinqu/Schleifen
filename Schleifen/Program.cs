040e9d8ffe78a1eda89a6d097386334a1e0e8618
student 04 <stk_lum_04@outlook.de>
2019-10-02 15:05:12 +0200

    Schleife

--- Schleifen/Program.cs
+++ Schleifen/Program.cs
@@ -10,6 +10,48 @@ namespace Schleifen
     {
         static void Main(string[] args)
         {
+            // //for(Variable, Bedingung, Schritte)
+            //// for (int i = 10; i>=20; i++)
+            // //{
+            //   //  Console.WriteLine("Durchlauf {0}", i);
+            // //}
+            // //while Schleife
+            // //while(Bedingung)
+            // int a = 1;
+            // while (a < 32)
+            // {
+            //     a = a * 2;
+            //     Console.WriteLine(a);
+            // }
+
+            // //do while Schleife
+
+            // //do Befehle while (Bedingung)
+            // int b = 1;
+            // do
+            // {
+            //     b = b * 2;
+            //     Console.WriteLine(b);
+            // }
+            // while (b < 32);
+
+
+            int a, b, r;
+            Console.WriteLine("Der ggT von zwei ganzen Zahlen soll berechnet werden");
+            Console.WriteLine("Erste Zahl eingeben");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Zweite Zahl eingeben");
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+            r = 1;
+            while(r !=0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            Console.WriteLine("Der ggT ist:{0}",a);
         }
     }
 }
